607d1a84a059928a241316c4faf5ce9baca612c1
mattin-tasbihgoo <mtasbihgoo1@hwemail.com>
2024-10-02 15:16:35 -0700

    fixed lead scouting and added passing variable

--- lib/scouting_form.dart
+++ lib/scouting_form.dart
@@ -15,6 +15,8 @@ bool tryParkAuto = false;
 int missedCone = 0;
 int missedCube = 0;
 
+int numberOfPasses = 0;
+
 class ScoutingForm extends StatefulWidget {
   const ScoutingForm({super.key, required this.teamName});
   final String teamName;
@@ -100,6 +102,20 @@ class _ScoutingForm extends State<ScoutingForm> {
     else if (score == "missed"){
       if (missedCube > 0) setState(() => missedCube -= 1);
     }
+  }
+
+    void _addPass() {
+    setState(() {
+      numberOfPasses++;
+    });
+  }
+
+  void _minusPass() {
+    setState(() {
+      if (numberOfPasses > 0) {
+        numberOfPasses--;
+      }
+    });
   }
   
 
