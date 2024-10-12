b6703e86821cd4cfb4a1f56cb7cfdcd760c1be13
john-xu-1 <63273190+john-xu-1@users.noreply.github.com>
2024-10-11 20:09:14 -0700

    not loading but added things to team display

--- lib/team_display_choice.dart
+++ lib/team_display_choice.dart
@@ -26,8 +26,10 @@ class _TeamDisplayChoiceState extends State<TeamDisplayChoice> {
 
   
   List<String> allMetricsName = List.empty(growable: true);
-  //String selectedMetric = "";
   
+  List<String> teamDataNames = List.empty(growable: true);
+  Map<String, Map<String, double>> teamsNumberDatas = {};
+  Map<String, Map<String, List<String>>> teamsNotesDatas = {};
 
 
   Future<void> _updateTeams() async {
@@ -40,11 +42,7 @@ class _TeamDisplayChoiceState extends State<TeamDisplayChoice> {
         allTeamColors[allTeams[i]] = Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0);
       }
       print (allTeamColors);
-      setState(() {
-        // for (int i = 0; i < allTeams.length; i++) {
-        //   allTeamDisplays.add(TeamDisplayInstance(teamID: allTeams[i]));
-        // }
-      }); //referesh after successful fetch
+      setState(() {});
       print(allTeams.length);
     } catch (e) {
       print('Error: $e');
@@ -100,12 +98,8 @@ class _TeamDisplayChoiceState extends State<TeamDisplayChoice> {
               
             }
             rutro.add( aMatch);
-            //print (aMatch);
           }
         }
-        // print (allTeams[k]);
-        // print (rutro);
-        // print ("====");
         if (all.containsKey(allTeams[k])) {
           print ("here");
           all.update(allTeams[k], (value) => rutro);
@@ -130,6 +124,39 @@ class _TeamDisplayChoiceState extends State<TeamDisplayChoice> {
     });
   }
 
+  Future<void> _updateTeamInfo(String team) async {
+    final sheet = await SheetsHelper.sheetSetup("Team Data");
+
+    await sheet!.values.insertRow(1, [team]);
+
+    final rows = await sheet!.values.allRows();
+
+    List<String> dataNames = rows[0].sublist(1);
+      
+    teamDataNames = dataNames; 
+    List<String> dataNumbers = rows[1].sublist(1);
+
+    teamsNumberDatas.putIfAbsent(team, () => {});
+
+
+    for (int i = 0; i < dataNames.length; i++)
+    {
+      if (isNumeric( dataNumbers[i])){
+        teamsNumberDatas[team]!.putIfAbsent(dataNames[i], () => double.parse(dataNumbers[i]));
+      }
+      else{
+        teamsNumberDatas[team]!.putIfAbsent(dataNames[i], () => 0);
+      }
+    }
+
+    setState(() {
+      
+    });
+
+    print (teamsNumberDatas);
+
+  }
+
   
   
 
@@ -139,8 +166,9 @@ class _TeamDisplayChoiceState extends State<TeamDisplayChoice> {
     double width = MediaQuery.of(context).size.width;
     TextEditingController teamDrop = TextEditingController();
     TextEditingController metricDrop = TextEditingController();
-    
 
+    //_updateTeamInfo("9442");
+    
     if (allTeams.isEmpty || all.isEmpty) {
       return const SafeArea(
         child: Scaffold(
@@ -159,113 +187,142 @@ class _TeamDisplayChoiceState extends State<TeamDisplayChoice> {
         title: const Text("Home"),
       ),
       body: Center(
-        child: Column(
-          children: [
-            Text(selectedTeamsDisplay),
-            Row(mainAxisAlignment: MainAxisAlignment.center ,children: allTeamColorsDisplay,),
-            Text(selectedMetric),
-            DropdownMenu<String>(
-                width: width,
-                hintText: "Select Teams To Compare",
-                requestFocusOnTap: true,
-                controller: teamDrop,
-                enableFilter: true,
-                label: const Text('Select Teams To Compare'),
-                onSelected: (String? value) {
-                  // setState(() {
-                  //   teamDrop.text = "";
-                  // });
-                  if (!selectedTeams.contains(value)) {
-
-                    setState(() {
-                      selectedTeamsDisplay += "$value, ";
-                      selectedTeams.add (value!);
-                      allTeamColorsDisplay.add(Icon(Icons.circle, color: allTeamColors[value],));
-                      allTeamColorsDisplay.add(const SizedBox(width: 10,),);
-                    });
-
-                    if (selectedMetric != ""){
-                      LineChartBarData line;
-                      print (all[value]);
-                      List<FlSpot> spots = List.empty(growable: true);
-                      for (int j = 0; j < all[value]!.length; j++){
-                        FlSpot spot = FlSpot(all[value]![j][selectedMetric!]![0] as double, all[value]![j][selectedMetric!]![1] as double);
-                        spots.add(spot);
-                        
-                      }
-                      line = LineChartBarData(spots: spots, color: allTeamColors[value]);
-                      //print (line);
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(selectedTeamsDisplay),
+              Row(mainAxisAlignment: MainAxisAlignment.center ,children: allTeamColorsDisplay,),
+              Text(selectedMetric),
+              DropdownMenu<String>(
+                  width: width,
+                  hintText: "Select Teams To Compare",
+                  requestFocusOnTap: true,
+                  controller: teamDrop,
+                  enableFilter: true,
+                  label: const Text('Select Teams To Compare'),
+                  onSelected: (String? value) {
+                    // setState(() {
+                    //   teamDrop.text = "";
+                    // });
+                    if (!selectedTeams.contains(value)) {
+          
                       setState(() {
-                        coordinates.add(line);
+                        selectedTeamsDisplay += "$value, ";
+                        selectedTeams.add (value!);
+                        allTeamColorsDisplay.add(Icon(Icons.circle, color: allTeamColors[value],));
+                        allTeamColorsDisplay.add(const SizedBox(width: 10,),);
                       });
-                    }
-                  }
-                  
-                },
-                dropdownMenuEntries:
-                    allTeams.map<DropdownMenuEntry<String>>((String menu) {
-                  return DropdownMenuEntry<String>(
-                      value: menu,
-                      label: menu,);
-                }).toList(),
-            ),
-            DropdownMenu<String>(
-                width: width,
-                hintText: "Select Metrics to Compare",
-                requestFocusOnTap: true,
-                controller: metricDrop,
-                enableFilter: true,
-                label: const Text('Select Metrics to Compare'),
-                onSelected: (String? value) {
-                  //coordinates = List.empty(growable: true);
-                  // setState(() {
-                  //   metricDrop.text = "";
-                  // });
-                  selectedMetric = value!;
-                  coordinates = List.empty(growable: true);
-                  try{
-                    for (int i = 0; i < selectedTeams.length; i++){
-                      LineChartBarData line;
-                      print (all[selectedTeams[i]]);
-                      List<FlSpot> spots = List.empty(growable: true);
-                      for (int j = 0; j < all[selectedTeams[i]]!.length; j++){
-                        if (all[selectedTeams[i]]![j][value!] != null){
-                          FlSpot spot = FlSpot(all[selectedTeams[i]]![j][value!]![0] as double, all[selectedTeams[i]]![j][value!]![1] as double);
+          
+                      if (selectedMetric != ""){
+                        LineChartBarData line;
+                        print (all[value]);
+                        List<FlSpot> spots = List.empty(growable: true);
+                        for (int j = 0; j < all[value]!.length; j++){
+                          FlSpot spot = FlSpot(all[value]![j][selectedMetric!]![0] as double, all[value]![j][selectedMetric!]![1] as double);
                           spots.add(spot);
+                          
                         }
-                        
+                        line = LineChartBarData(spots: spots, color: allTeamColors[value]);
+                        //print (line);
+                        setState(() {
+                          coordinates.add(line);
+                        });
                       }
-                      line = LineChartBarData(spots: spots, color: allTeamColors[selectedTeams[i]]);
-                      //print (line);
-                      setState(() {
-                        coordinates.add(line);
-                      });
                     }
-                  }
-                  catch(e){
-                    print (e);
-                  }
-                  
-                },
-                dropdownMenuEntries:
-                    allMetricsName.sublist(2).map<DropdownMenuEntry<String>>((String menu) {
-                  return DropdownMenuEntry<String>(
-                      value: menu,
-                      label: menu,);
-                }).toList(),
-            ),
-            coordinates.isNotEmpty ? 
-            SizedBox(
-              height: height*0.5,
-              width: width,
-              child: LineChart(
-                LineChartData(
-                  lineBarsData: coordinates,
+                    
+                  },
+                  dropdownMenuEntries:
+                      allTeams.map<DropdownMenuEntry<String>>((String menu) {
+                    return DropdownMenuEntry<String>(
+                        value: menu,
+                        label: menu,);
+                  }).toList(),
+              ),
+              DropdownMenu<String>(
+                  width: width,
+                  hintText: "Select Metrics to Compare",
+                  requestFocusOnTap: true,
+                  controller: metricDrop,
+                  enableFilter: true,
+                  label: const Text('Select Metrics to Compare'),
+                  onSelected: (String? value) {
+                    //coordinates = List.empty(growable: true);
+                    // setState(() {
+                    //   metricDrop.text = "";
+                    // });
+                    selectedMetric = value!;
+                    coordinates = List.empty(growable: true);
+                    try{
+                      for (int i = 0; i < selectedTeams.length; i++){
+                        LineChartBarData line;
+                        print (all[selectedTeams[i]]);
+                        List<FlSpot> spots = List.empty(growable: true);
+                        for (int j = 0; j < all[selectedTeams[i]]!.length; j++){
+                          if (all[selectedTeams[i]]![j][value!] != null){
+                            FlSpot spot = FlSpot(all[selectedTeams[i]]![j][value!]![0] as double, all[selectedTeams[i]]![j][value!]![1] as double);
+                            spots.add(spot);
+                          }
+                          
+                        }
+                        line = LineChartBarData(spots: spots, color: allTeamColors[selectedTeams[i]]);
+                        //print (line);
+                        setState(() {
+                          coordinates.add(line);
+                        });
+                      }
+                    }
+                    catch(e){
+                      print (e);
+                    }
+                    
+                  },
+                  dropdownMenuEntries:
+                      allMetricsName.sublist(2).map<DropdownMenuEntry<String>>((String menu) {
+                    return DropdownMenuEntry<String>(
+                        value: menu,
+                        label: menu,);
+                  }).toList(),
+              ),
+              coordinates.isNotEmpty ? 
+              SizedBox(
+                height: height*0.5,
+                width: width,
+                child: LineChart(
+                  LineChartData(
+                    lineBarsData: coordinates,
+                  ),
+                ),
+              )
+              : const SizedBox(),
+              SizedBox(
+                height: height * 0.75,
+                child: ListView.separated(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: selectedTeams.length,
+                  itemBuilder: (BuildContext context, int index) 
+                  {
+                    print (selectedTeams);
+                    if (!teamsNumberDatas.containsKey(selectedTeams[index])) _updateTeamInfo(selectedTeams[index]);
+                    return IconButton(
+                      onPressed: (){
+                        Navigator.push(
+                          context, 
+                          MaterialPageRoute
+                          (
+                            builder: (context) => TeamDisplayInstance(teamDataNames: teamDataNames, teamsNumberDatas: teamsNumberDatas, team: selectedTeams[index])
+                          )
+                        );
+                      },
+                      //color: allTeamColors[selectedTeams[index]],
+                      icon: Row( children: [ Icon(Icons.circle, color: allTeamColors[selectedTeams[index]]), Text(selectedTeams[index]) ] ),
+                    );
+                    //TeamDisplayInstance(teamDataNames: teamDataNames, teamsNumberDatas: teamsNumberDatas, team: selectedTeams[index]);
+                  },
+                  separatorBuilder: (BuildContext context, int index) => const Divider()
                 ),
               ),
-            )
-            : SizedBox()
-          ],
+            ],
+          ),
         )
       ),
       floatingActionButton: FloatingActionButton(
--- lib/team_display_instance.dart
+++ lib/team_display_instance.dart
@@ -1,234 +1,47 @@
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
 import 'package:frc1148_2023_scouting_app/scouting_form.dart';
-import 'package:frc1148_2023_scouting_app/sheets_helper.dart';
 import 'package:frc1148_2023_scouting_app/color_scheme.dart';
 
-
-import 'package:fl_chart/fl_chart.dart';
 
 List<ScoutingForm> sf = List.empty();
 
 class TeamDisplayInstance extends StatefulWidget {
-  const TeamDisplayInstance({super.key, required this.teamID});
-  final String teamID;
+  const TeamDisplayInstance({super.key, required this.teamDataNames, required this.teamsNumberDatas, required this.team, });
+
+  final String team;
+  final List<String> teamDataNames;
+  final Map<String, Map<String, double>> teamsNumberDatas;
+  
   @override
   State<TeamDisplayInstance> createState() => _TeamDisplayInstanceState();
 }
 
 class _TeamDisplayInstanceState extends State<TeamDisplayInstance> {
-  List<String> dataSet = List.empty();
-  List<String> dataNames = List.empty();
-
-  Future<void> _getNames() async {
-    try {
-      final sheet = await SheetsHelper.sheetSetup("TeamDisplay");
-
-      dataNames = await sheet!.values.row(1);
-      dataNames = dataNames.sublist(1);
-      setState(() {}); //referesh after successful fetch
-    } catch (e) {
-      print('Error: $e');
-    }
-  }
-
-  Future<void> _updateTeams() async {
-    try {
-      // final gsheets = GSheets(_creds);
-      // final ss = await gsheets.spreadsheet('1C4_kygqZTOo3uue3eBxrMV9b_3UJVuDiOVZqAeGHvzE');
-      // final sheet = ss.worksheetByTitle('TeamDisplay');
-
-      final sheet = await SheetsHelper.sheetSetup("TeamDisplay");
-
-      // Writing data
-      //final firstRow = [topScoreCone, midScoreCone, lowScoreCone, topScoreCube, midScoreCube, lowScoreCube, tryParkAuto, missedCube];
-      //await sheet!.values.insertRowByKey (widget.teamName, firstRow, fromColumn: 2);
-      // prints [index, letter, number, label]
-
-      dataSet = (await sheet!.values.rowByKey(widget.teamID))!;
-      dataSet = dataSet.sublist(0);
-      setState(() {}); //referesh after successful fetch
-    } catch (e) {
-      print('Error: $e');
-    }
-  }
-
-  //HashMap<String, List<int>> rutro = HashMap();
-  List<Map<String, List<int>>> rutro = List.empty(growable: true);
-
-
-  Future<void> _updateGraphs() async {
-    try {
-
-      final sheet = await SheetsHelper.sheetSetup("App results");
-
-      final rows = await sheet!.values.allRows();
-      
-      final allMetricsName = rows[0];
-
-      for (int i = 1; i < rows.length; i++){ 
-        int spaceLoc = rows[i][0].indexOf(" ");
-        String teamNumber = rows[i][0].substring(spaceLoc+4);
-        String matchNumber = rows[i][0].substring(1, spaceLoc);
-
-        if (teamNumber == widget.teamID){
-          final allMetricsInAMatch = rows[i];
-          
-          //List<List<int>>> coordinates = List.empty(growable: true);
-
-          Map<String, List<int>> aMatch = {};
-
-          for (int j = 2; j < allMetricsInAMatch.length; j++){
-            //sus
-            
-            if (allMetricsInAMatch[j] != "true" && allMetricsInAMatch[j] != "false"){
-              
-              String columnName = allMetricsName[j];
-              List<int> coordinate = [int.parse(matchNumber), int.parse(allMetricsInAMatch[j])];
-              if (aMatch.containsKey(columnName)) {
-                aMatch.update(columnName, (value) => coordinate);
-              } else {
-                aMatch.putIfAbsent(columnName, () => coordinate);
-              }
-            }
-            
-          }
-          rutro.add( aMatch);
-        }
-      }
-
-      //print (rutro);
-
-      // dataSet = (await sheet!.values.rowByKey(widget.teamID))!;
-      // dataSet = dataSet.sublist(0);
-      setState(() {}); 
-    } catch (e) {
-      print('Error: $e');
-    }
-  }
+  
 
   @override
   void initState() {
     super.initState();
 
-    _getNames();
-    _updateTeams();
-    _updateGraphs();
   }
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    List<FlSpot> coordinates = List.empty(growable: true);
-    String selected = "Amp Points";
-    for (int i = 0; i < rutro.length; i++){
-      FlSpot spot = FlSpot(rutro[i][selected]![0] as double, rutro[i][selected]![1] as double);
-      coordinates.add(spot);
-    }
-    
-    print (coordinates);
-    if (dataSet.isEmpty || dataNames.isEmpty) {
-      return const SafeArea(
-        child: Scaffold(
-            backgroundColor: colors.myBackground,
-            body: Center(
-              child: Text(
-                "loading ...",
-                style: TextStyle(color: colors.myOnPrimary),
-              ),
-            )),
-      );
-    }
-
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.teamID),
-        ),
-        body: Center(
-          child: LineChart(
-            LineChartData(
-              lineBarsData: [
-                LineChartBarData(
-                  spots: coordinates
-                )
-              ]
-            ),
-
-          )
-        )
-        // body: Center(
-        //   child: ListView.separated(
-        //     padding: const EdgeInsets.all(8),
-        //     itemCount: dataSet.length,
-        //     itemBuilder: (BuildContext context, int index) {
-        //       return DataBlock(category: dataNames[index], data: dataSet[index]);
-        //   },
-        //   separatorBuilder: (BuildContext context, int index) =>
-        //     Container(
-        //       alignment: AlignmentDirectional.center,
-        //       height: height / 150,
-        //     ),
-        //   )
-        // )
-
-        // Column(
-        //   children: [
-        //     DataBlock(category: "driving speed", data: dataSet[0]),
-        //     DataBlock(category: "meow", data: dataSet[1]),
-        //   ]
-        // ),
-
+    return Container(
+      color: const Color.fromARGB(255, 30, 30, 30),
+      child: ListView.separated(
+        padding: const EdgeInsets.all(8),
+        itemCount: widget.teamDataNames.length,
+        itemBuilder: (BuildContext context, int jndex) {
+          return Container(
+            color: colors.mySecondaryColor,
+            height: 50,
+            alignment: Alignment.centerLeft,
+            child: Text("${widget.teamDataNames[jndex]}: ${widget.teamsNumberDatas[widget.team]![widget.teamDataNames[jndex]]!}", style: const TextStyle(color: colors.myPrimaryColor, fontSize: 20),)
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const SizedBox(height: 10,),
+      ),
     );
-  }
-}
-
-class DataBlock extends StatelessWidget {
-  const DataBlock({super.key, required this.category, required this.data});
-  final String category;
-  final String data;
-  @override
-  Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
-    return Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          height: height/13,
-           decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),
-            //color: Color.fromRGBO(86, 14, 12, 0.982)),
-            //color: Color.fromARGB(255, 45, 44, 44)),
-            color: colors.myOnBackgroundD),
-          child: Column(
-            children: [
-              //const Divider(),
-              SizedBox(
-                height: height/45,
-                width: width,
-              ),
-              Container(
-                //color: Color.fromARGB(255, 192, 180, 180),
-                width:width/2,
-                decoration:
-                  BoxDecoration(borderRadius: BorderRadius.circular(20),
-                  //color: Color.fromARGB(255, 204, 191, 191),),
-                  color: colors.myPrimaryColor,),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("$category: ",textScaleFactor: 1.5,style: TextStyle(color: colors.myOnPrimary)),
-                      if (data==("true"))
-                        Icon(Icons.check)
-                        else if (data==("false"))
-                        Icon(Icons.close)
-                        else
-                      Text(data,textScaleFactor: 1.5,style: TextStyle( color: const Color.fromARGB(255, 12, 11, 11))),
-                    ],
-                ),
-              ),
-            ],
-          )
-        )
-      );
   }
 }
